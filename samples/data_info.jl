00db86581d9e26a2c7a1509f8cb0bd1e63651858
moljac <mcvjetko@holisticware.net>
2020-10-17 19:54:39 +0200

    removed rename difles

--- samples/data_info.jl
+++
@@ -1,7 +0,0 @@
-using DataFrames, CSV, Statistics, StatsBase
-data = CSV.read("../../../externals/Core.Math.Samples/data/Pejcic_318.csv", copycols = true)
-
-println(size(data),"\n")
-println(names(data),"\n")
-println(first(data, 6),"\n")
-println(describe(data),"\n")
--- samples/data_types.jl
+++
@@ -1,6 +0,0 @@
-using DataFrames, CSV, Statistics, StatsBase
-data = CSV.read("../../../externals/Core.Math.Samples/data/Pejcic_318.csv", copycols = true)
-
-println(describe(data),"\n")
-println(names(data),"\n")
-println(eltype.(eachcol(data)),"\n")
