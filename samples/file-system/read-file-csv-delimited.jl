7b9cf8e793287dd9e0a15932708fa6c3fbcd2251
moljac <mcvjetko@holisticware.net>
2020-10-17 19:55:07 +0200

    variables with better names

--- samples/file-system/read-file-csv-delimited.jl
+++ samples/file-system/read-file-csv-delimited.jl
@@ -1,4 +1,4 @@
-fname = "../../../externals/Core.Math.Samples/data/Pejcic_318.csv"
+fname = "../externals/Core.Math.Samples/data/Pejcic_318.csv"
 data = readdlm(fname, separator = ',')
 
-data = CSV.read("Pejcic_318.csv", header=true, delim=',', copycols = true)
+data_read_02 = CSV.read("Pejcic_318.csv", header=true, delim=',', copycols = true)
