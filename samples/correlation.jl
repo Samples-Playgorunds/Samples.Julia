05354451fa1da77421330aaeae949c1ddecbcb17
katodix <darko.katovic@gmail.com>
2020-10-20 14:27:13 +0200

    Create Correlation.jl

---
+++ samples/correlation.jl
@@ -0,0 +1,10 @@
+import Pkg
+Pkg.instantiate() # downloads all dependencies for the current project
+
+Pkg.add("RDatasets")
+
+using DataFrames, CSV, RDatasets, Statistics
+
+df = CSV.read("../externals/Core.Math.Data/data/Pejcic_318.csv", copycols = true)
+
+println(Statistics.cor(df.ATV, df.ATT))
