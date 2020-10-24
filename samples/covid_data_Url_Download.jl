9d15afb2d0e7b8512bc2cae93e2a2ba95f9536b1
katodix <darko.katovic@gmail.com>
2020-10-24 09:51:07 +0200

    covid data url download

---
+++ samples/covid_data_Url_Download.jl
@@ -0,0 +1,18 @@
+import Pkg
+Pkg.instantiate()
+
+Pkg.add("UrlDownload")
+Pkg.add("CSV")
+Pkg.add("DataFrames")
+
+using UrlDownload, DataFrames, CSV
+
+url = "https://www.stats.govt.nz/assets/Uploads/Business-price-indexes/Business-price-indexes-December-2019-quarter/Download-data/business-price-indexes-december-2019-quarter-csv.csv"
+# df = urldownload(url)
+df = urldownload(url, true)
+
+res = urldownload(url, parser = x -> DataFrame(CSV.File(IOBuffer(x))))
+
+# url = "https://covidtracking.com/api/v1/states/current.csv"
+# res = urldownload(url, parser = x -> DataFrame(CSV.File(IOBuffer(x))))
+
