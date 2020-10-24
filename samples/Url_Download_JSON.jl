b97cdc0aa9a6ebd17d16a3c9cc6e5b9776b61734
katodix <darko.katovic@gmail.com>
2020-10-24 12:27:23 +0200

    Url_Download_JSON

--- samples/Url_Download_JSON.jl
+++ samples/Url_Download_JSON.jl
@@ -8,8 +8,11 @@ Pkg.add("DataFrames")
 using UrlDownload, DataFrames, CSV
 
 
+# url = "https://covidtracking.com/api/v1/states/current.json"
+# df = download(url,  "data/covid-current.json")
+
 url = "https://covidtracking.com/api/v1/states/current.json"
-df = download(url,  "data/covid-current.json")
+df = urldownload(url) |> DataFrame
 
 
 # url = "https://www.stats.govt.nz/assets/Uploads/Business-price-indexes/Business-price-indexes-December-2019-quarter/Download-data/business-price-indexes-december-2019-quarter-csv.csv"
