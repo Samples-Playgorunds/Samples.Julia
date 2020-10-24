5c9295c5540c7c581395dab37a2b349f5db02efd
katodix <darko.katovic@gmail.com>
2020-10-24 09:54:36 +0200

    buliding steps in progress

---
+++ samples/buliding_steps.jl
@@ -0,0 +1,48 @@
+import Pkg
+Pkg.instantiate()
+
+Pkg.add("CSV")
+Pkg.add("DataFrames")
+Pkg.add("Feather") # binary file forma
+Pkg.add("Gadfly")
+Pkg.add("IJulia")
+Pkg.add("JSON")
+Pkg.add("RDatasets")
+
+using CSV, DataFrames, Feather, Gadfly, IJulia, JSON, RDatasets
+
+# data = readdlm("iris_dataset.csv", ',')
+
+iris = CSV.read("iris_dataset.csv", header=true, delim=',', copycols = true)
+
+print(iris)
+
+print(describe(iris))
+
+# print(describe(iris, stats=[:q25, :q75, :first, :last]))
+
+# print(describe(iris, stats=:all))
+
+# Pkg.add("Statistics")
+
+using Statistics
+
+print(mean(iris[:sepallength]))
+
+for x in names(iris)[1:end-1]
+    for y in names(iris)[1:end-1]
+        println("$x /t $y /t $(cor(iris[x], iris[y]))")
+    end
+    println("---------------------------------------------")
+end
+
+# columns to array
+sepallength = Array(iris[:sepallength])
+# print(sepallength)
+
+# DataFrame to matrix
+irisarr = convert(Array, iris[:,:])
+
+iris[1:3, [:petallength, :petalwidth]] 
+
+# CSV.write("test_iris.csv", iris)
