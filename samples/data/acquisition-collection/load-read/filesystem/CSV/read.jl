a8a673a1c7e2d2dcb042ab749bc7995716748efa
moljac <mcvjetko@holisticware.net>
2020-10-27 14:50:05 +0100

    few variants of reading

--- samples/data/acquisition-collection/load-read/filesystem/CSV/read.jl
+++ samples/data/acquisition-collection/load-read/filesystem/CSV/read.jl
@@ -2,16 +2,55 @@ import Pkg
 Pkg.instantiate()
 Pkg.update()
 
-filename="../../../../../../externals/Core.Math.Data/data/Pejcic_318.csv"
+file_path_csv = "../../../../../../externals/Core.Math.Data/data/Pejcic_318.csv"
 
+#------------------------------------------------------------------------------------
+# low level file API
+# https://en.wikibooks.org/wiki/Introducing_Julia/Working_with_text_files
+file_handle = open(file_path_csv)
+
+totaltime, totallines = open(file_path_csv) do file_handle_1
+    linecounter = 0
+    timetaken = @elapsed for l in eachline(file_handle_1)
+        linecounter += 1
+    end
+    (timetaken, linecounter)
+end
+
+close(file_handle)
+
+open(file_path_csv) do file_handle_1
+    line = 1
+    while !eof(file_handle_1)
+      x = readline(file_handle_1)
+      println("$line $x")
+      line += 1
+    end
+  end
+
+  open(file_path_csv) do file_handle_1
+    for i in enumerate(eachline(file_handle_1))
+        println(i[1], ": ", i[2])
+      end
+  end
+
+  data_as_string = read(file_path_csv, String)
+
+#------------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------------
+# high level CSV API
 # https://juliadata.github.io/CSV.jl/stable/index.html
+# https://www.geeksforgeeks.org/working-with-csv-files-in-julia/
 Pkg.add("CSV")
 using CSV
 
-data = CSV.read(filename, copycols = true)
+data = CSV.read(file_path_csv, copycols = true)
 
 Pkg.add("FileIO")
 Pkg.add("CSVFiles")
 using FileIO, CSVFiles, DataFrames
 
-load(filename, spacedelim=true, header_exists=false) |> DataFrame
+data_frame_csv_api = load(file_path_csv, spacedelim=true, header_exists=false) |> DataFrame
+#------------------------------------------------------------------------------------
