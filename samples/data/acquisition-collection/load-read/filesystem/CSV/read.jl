e7df56d653e16bca42e021cb7f57d67b30fddc00
moljac <mcvjetko@holisticware.net>
2020-10-27 16:56:14 +0100

    print stuff

--- samples/data/acquisition-collection/load-read/filesystem/CSV/read.jl
+++ samples/data/acquisition-collection/load-read/filesystem/CSV/read.jl
@@ -48,9 +48,27 @@ using CSV
 
 data = CSV.read(file_path_csv, copycols = true)
 
+println(size(data),"\n")
+println(names(data),"\n")
+println(first(data, 6),"\n")
+# println(describe(data),"\n")
+
 Pkg.add("FileIO")
 Pkg.add("CSVFiles")
 using FileIO, CSVFiles, DataFrames
 
-data_frame_csv_api = load(file_path_csv, spacedelim=true, header_exists=false) |> DataFrame
+data_frame_csv_api_1 = load(file_path_csv, spacedelim=true, header_exists=false) |> DataFrame
+
+println(size(data_frame_csv_api_1),"\n")
+println(names(data_frame_csv_api_1),"\n")
+println(first(data_frame_csv_api_1, 6),"\n")
+# println(describe(data_frame_csv_api),"\n")
+
+data_frame_csv_api_2 = load(file_path_csv) |> DataFrame
+
+println(size(data_frame_csv_api_2),"\n")
+println(names(data_frame_csv_api_2),"\n")
+println(first(data_frame_csv_api_2, 6),"\n")
+# println(describe(data_frame_csv_api),"\n")
+
 #------------------------------------------------------------------------------------
