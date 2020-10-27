122486b0fd18db81c5fa0dfeed75f81c9fbb5a90
katodix <darko.katovic@gmail.com>
2020-10-27 12:49:27 +0100

    HTTP get

---
+++ samples/HTTP_get.jl
@@ -0,0 +1,23 @@
+# https://dev.solita.fi/2020/04/07/trying-out-julia.html #
+
+import Pkg
+Pkg.instantiate() # downloads all dependencies for the current project
+
+Pkg.add("JSON")
+Pkg.add("HTTP")
+Pkg.add("Plots")
+Pkg.add("StatsPlots")
+
+using HTTP, JSON, Plots, StatsPlots
+
+resp = HTTP.get("https://api.covid19api.com/country/finland/status/confirmed")
+str = String(resp.body)
+jobj = JSON.Parser.parse(str)
+
+println(jobj[1])
+
+cases = map(x -> get(x, "Cases", 0), jobj)
+plot(cases)
+
+savefig("cases_simple.png") #Save plot as png image
+
